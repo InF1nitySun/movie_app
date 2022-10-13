e7c2a15c40932f6185ee3d29e9fcf2ea3b822610
TostTJ <tosttj@mail.com>
2022-10-13 15:47:05 +0600

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+
+import items from './components/Items'
+
+const Movie = ({title, url, thumbnailUrl, rating}) => {
+
+    return (
+        <div style={{display: "block"}}>
+            <img src={url} alt={title} style={{width: "100px"}}/>
+            <p>{title}</p>
+            <a href={thumbnailUrl}>Link</a>
+            <p>{rating} / 5.0</p>
+        </div>
+    )
+}
+
+Movie.prototype = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    thumbnailUrl: PropTypes.string,
+    rating: PropTypes.number
+}
+
+const RenderItems = () => items.map(item => <Movie key={item.id} {...item}/>)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <RenderItems/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,32 @@
+const items = [
+    {
+        "id": 1,
+        "title": "accusamus beatae ad facilis cum similique qui sunt",
+        "url": "https://via.placeholder.com/600/92c952",
+        "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+        "rating": 4.8
+    },
+    {
+        "id": 2,
+        "title": "reprehenderit est deserunt velit ipsam",
+        "url": "https://via.placeholder.com/600/771796",
+        "thumbnailUrl": "https://via.placeholder.com/150/771796",
+        "rating": 4.3
+    },
+    {
+        "id": 3,
+        "title": "officia porro iure quia iusto qui ipsa ut modi",
+        "url": "https://via.placeholder.com/600/24f355",
+        "thumbnailUrl": "https://via.placeholder.com/150/24f355",
+        "rating": 2.8
+    },
+    {
+        "id": 4,
+        "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+        "url": "https://via.placeholder.com/600/d32776",
+        "thumbnailUrl": "https://via.placeholder.com/150/d32776",
+        "rating": 4.1
+    }
+]
+
+export default items
